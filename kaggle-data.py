d9b2bf1b3c36243c46f965413dc4a2e3e0cc7dca
peterphilippine <peterphilippine@gmail.com>
2025-09-30 19:58:45 +0800

    Update kaggle-data.py

--- kaggle-data.py
+++ kaggle-data.py
@@ -107,10 +107,10 @@ class MambaSampler(Sampler):
                 self.pos = int(f.read().strip())
         else:
             self.pos = 0
-        if "val_indices.txt" in self.indices_file and self.pos >= len(self.available_indices):
-            self.pos = 0
 
     def __iter__(self):
+        if "val_indices.txt" in self.indices_file and self.pos >= len(self.available_indices):
+            self.pos = 0
         while self.pos < len(self.available_indices):
             idx = self.available_indices[self.pos]
             self.pos += 1
